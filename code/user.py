94294671b705082ee9594eb1db2d0e79bb76efc0
Yamila Molina <yamila.molina@ibm.com>
2021-11-10 17:11:31 -0800

    CRUD completo

--- code/user.py
+++ code/user.py
@@ -1,4 +1,6 @@
 import sqlite3
+from sqlite3.dbapi2 import Cursor, connect
+from flask_restful import Resource, reqparse
 
 class User:
     def __init__(self, _id, username, password):
@@ -39,3 +41,34 @@ class User:
 
         connection.close()
         return user
+
+class UserRegister(Resource):
+
+    parser = reqparse.RequestParser()
+    parser.add_argument('username', 
+        type=str, 
+        required=True, 
+        help="Este campo no puede dejarse vacío."
+    )
+    parser.add_argument('password', 
+        type=str, 
+        required=True, 
+        help="Este campo no puede dejarse vacío."
+    )
+
+    def post(self):
+        data = UserRegister.parser.parse_args()
+        
+        if User.find_by_username(data['username']):
+            return {"message": "Ya existe un usuario con ese username."}, 400
+
+        connection = sqlite3.connect("data.db")
+        cursor = connection.cursor()
+
+        query = "INSERT INTO user VALUES (NULL, ?, ?)"
+        cursor.execute(query, (data['username'], data['password'])) #siempre tupla
+
+        connection.commit()
+        connection.close()
+
+        return {"message": "El usuario se creó correctamente."}, 201
